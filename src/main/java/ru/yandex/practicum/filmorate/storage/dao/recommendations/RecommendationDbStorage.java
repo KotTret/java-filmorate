ee330cbb93573bd1a47514e29ab427ba9abdfd01
UPRTru <uprt.mi2@yandex.ru>
2022-12-27 09:59:12 +0300

    fix getRecommendationsFilms

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/recommendations/RecommendationDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/recommendations/RecommendationDbStorage.java
@@ -2,37 +2,33 @@ package ru.yandex.practicum.filmorate.storage.dao.recommendations;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.RecommendationStorage;
 import ru.yandex.practicum.filmorate.storage.dao.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.dao.genre.GenreDAO;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class RecommendationDbStorage implements RecommendationStorage {
     private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate nameJdbcTemplate;
 
     @Override
     public List<Integer[]> getAllLikes() {
-        List<Integer[]> allLikes = new ArrayList<>();
-        jdbcTemplate.query("SELECT * FROM film_likes", (ResultSet rs) -> {
-            Integer[] like = {rs.getInt("film_id"), rs.getInt("user_id")};
-            allLikes.add(like);
-            while (rs.next()) {
-                like = new Integer[]{rs.getInt("film_id"), rs.getInt("user_id")};
-                allLikes.add(like);
-            }
-        });
+        List<Integer[]> allLikes = jdbcTemplate.query("SELECT * FROM film_likes", RecommendationDbStorage::allLikes);
         return allLikes;
+    }
+    private static Integer[] allLikes(ResultSet resultSet, int rowNum) throws SQLException {
+        return new Integer[]{resultSet.getInt("film_id"), resultSet.getInt("user_id")};
     }
 
     @Override
@@ -43,31 +39,13 @@ public class RecommendationDbStorage implements RecommendationStorage {
                 "FROM films, mpa " +
                 "WHERE films.mpa_id = mpa.mpa_id " +
                 "AND films.film_id IN (" + id + ")";
-        final String genre = "SELECT genres.genre_id, genres.genre_name, film_genres.film_id " +
-                "FROM films, genres, film_genres " +
-                "WHERE films.film_id = film_genres.film_id " +
-                "AND film_genres.genre_id = genres.genre_id " +
-                "AND films.film_id = ? ";
-        Set<Film> setFilm = new HashSet<>();
-        jdbcTemplate.query(filmList, (ResultSet rs) -> {
-            Film film = FilmDbStorage.mapRowToFilm(rs, rs.getRow());
-            List<Genre> genres = (jdbcTemplate.query(genre, RecommendationDbStorage::mapRowToGenre, rs.getInt("film_id")));
-            film.getGenres().addAll(genres);
-            setFilm.add(film);
-            while (rs.next()) {
-                film = FilmDbStorage.mapRowToFilm(rs, rs.getRow());
-                genres = (jdbcTemplate.query(genre, RecommendationDbStorage::mapRowToGenre, rs.getInt("film_id")));
-                film.getGenres().addAll(genres);
-                setFilm.add(film);
-            }
-        });
-        return new ArrayList<>(setFilm);
-    }
-
-    public static Genre mapRowToGenre(ResultSet resultSet, int rowNum) throws SQLException {
-        return Genre.builder()
-                .id(resultSet.getInt("genre_id"))
-                .name(resultSet.getString("genre_name"))
-                .build();
+        Map<Integer, Film> films = new HashSet<>(jdbcTemplate.query(filmList, FilmDbStorage::mapRowToFilm))
+                .stream().collect(Collectors.toMap(Film::getId, film -> film));
+        SqlParameterSource parameters = new MapSqlParameterSource("idFilms", films.keySet());
+        String genres = "SELECT fg.film_id, fg.genre_id, g.genre_name from FILM_GENRES as fg " +
+                "LEFT JOIN genres AS g ON  fg.genre_id = g.genre_id where film_id IN (:idFilms)";
+        nameJdbcTemplate.query(genres, parameters,
+                (rs, rowNum) -> films.get(rs.getInt("film_id")).getGenres().add(GenreDAO.mapRowToGenre(rs, rowNum)));
+        return new ArrayList<>(films.values());
     }
 }
